b8117dde0a0c1420d050d5b91307a5e0accc7b23
raghav <raghavgupta3383@gmail.com>
2022-07-11 23:16:26 +0530

    raghav, aditya, nikhil

---
+++ final_trial/trial_check/src/components/menu/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../../assets/css/footer.css';
+
+const Footer = () => (
+  <div className="footer page-footer font-small blue">
+    <div className="custom-footer text-center py-3">© 2018 Copyright:
+      <p> Raghav Gupta, Nikhil, Aditya Nagyal</p>
+    </div>
+  </div>
+);
+
+export default Footer;
---
+++ final_trial/trial_check/truffle.js
@@ -0,0 +1,13 @@
+'use strict';
+const fullPathBuildDirectory = `${__dirname}/src/assets/abis`;
+
+module.exports = {
+    contracts_build_directory: fullPathBuildDirectory,
+    networks: {
+        development: {
+            host: '127.0.0.1',
+            port: 8545,
+            network_id: '*'
+        }
+    }
+};
